0cb76f70d320bafe0236d0242fc09c2f9395bf6d
mrbseba <eusebiu.barimb11@gmail.com>
2022-10-07 23:19:44 +0300

    try to commit the first change

---
+++ firstProjectAVD.py
@@ -0,0 +1,12 @@
+# Data analysis and visualization with machine learning
+# A. An introduction to machine learning with sickit-learn
+# use: NumPy, SciPy, matplotlib
+
+# import the liray of the project
+from sklearn import datasets
+iris = datasets.load_iris()
+digits = datasets.load_digits()
+
+print(digits.data)
+
+
